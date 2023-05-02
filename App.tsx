c159d92c458e1154ade4f7d2dc7a59423c454de3
Pulkit Goyal <7895pulkit@gmail.com>
2023-05-02 19:49:05 +0530

    ios related stuff

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import { HekaHealthComponent } from '@heka-health/heka-react-native'
+import { HekaComponent } from '@heka-health/heka-react-native'
 import { AppKey, UserUUID } from './constants'
 
 export default function App() {
-  return <HekaHealthComponent appKey={AppKey} userUUID={UserUUID} />
+  return <HekaComponent appKey={AppKey} userUUID={UserUUID} />
 }
--- ios/hekareactnativeclient/AppDelegate.h
+++ ios/hekareactnativeclient/AppDelegate.h
@@ -1,7 +1,11 @@
 #import <RCTAppDelegate.h>
 #import <UIKit/UIKit.h>
 #import <Expo/Expo.h>
+#import "RNAppAuthAuthorizationFlowManager.h"
 
-@interface AppDelegate : EXAppDelegateWrapper
+// added the <RNAppAuthAuthorizationFlowManager>
+@interface AppDelegate : EXAppDelegateWrapper <RNAppAuthAuthorizationFlowManager>
+
+@property(nonatomic, weak)id<RNAppAuthAuthorizationFlowManagerDelegate>authorizationFlowManagerDelegate;
 
 @end
